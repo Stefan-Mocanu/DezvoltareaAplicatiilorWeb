e2f9f907cb52219884d8dc3f5adaeb34b9b79063
Stefan-Mocanu <stefanmo.sm@gmail.com>
2023-11-03 20:13:43 +0200

    Partea1
    Adaugare descriere proiect

---
+++ description.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Managerierea unui hotel: Descriere proiect.</h1>
+    <h2>Mocanu Ștefan 233</h2>
+    <p>Aplicatia va permite conectarea la conturi specifice clientilor sau a personalului care se ocupa de curatenie.<br>
+        Prima categorie de conturi permite utilizatorilor de a-si rezerva o camera din hotel, sa-si vada rezervarile.<br>
+        Angajatii pot vedea planificarea curateniei pe etajele la care sunt repartizati intr-o anumita zi, sau ce camere care s-au eliberat trebuie sa le curete.<br>
+    </p>
+    <h3>Diagrama UML</h3>
+    <img src="Media/UML.png" alt="Diagrama lispseste=(((">
+</body>
+</html>
